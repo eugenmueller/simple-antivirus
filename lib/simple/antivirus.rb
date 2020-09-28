1b8e190b0575e7ed362f8a6a51489f96ce6fc20a
Eugen Müller <eugen.mueller@enermarket.de>
2020-09-28 15:16:14 +0200

    Initial commit.

---
+++ lib/simple/antivirus.rb
@@ -0,0 +1,8 @@
+require "simple/antivirus/version"
+
+module Simple
+  module Antivirus
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/simple/antivirus_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Simple::Antivirus do
+  it "has a version number" do
+    expect(Simple::Antivirus::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
